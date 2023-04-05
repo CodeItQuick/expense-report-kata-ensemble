f7ef0f074da412055aac6923e8adbc7903d4afe4
Ted M. Young <tedyoung@gmail.com>
2023-04-05 14:07:53 -0700

    F

--- src/test/java/dev/ted/kata/ExpenseReportTest.java
+++ src/test/java/dev/ted/kata/ExpenseReportTest.java
@@ -1,11 +1,14 @@
 package dev.ted.kata;
 
+import com.github.larseckart.tcr.TestCommitRevertExtension;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.*;
 
+@ExtendWith(TestCommitRevertExtension.class)
 public class ExpenseReportTest {
 
     @Test
